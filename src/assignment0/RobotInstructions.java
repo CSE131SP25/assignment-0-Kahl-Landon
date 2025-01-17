6a697d456a360f6c08e5cfd55377a6a16746f7c6
landonkahl <landonkahl@localhost>
2025-01-17 15:09:12 -0600

    I drew a flag

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,28 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
 		robot.turnLeft(90);
-		robot.forward(23);
+		robot.forward(500);
+		robot.turnRight(180);
+		robot.forward(1000);
+		robot.turnLeft(180);
+		robot.forward(500);
 		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(400);
+		robot.turnRight(90);
+		robot.forward(132);
+		robot.turnRight(90);
+		robot.forward(75);
+		robot.turnRight(90);
+		robot.forward(132);
+		robot.turnRight(180);
+		robot.forward(44);
+		robot.turnLeft(90);
+		robot.forward(75);
+		robot.turnRight(90);
+		robot.forward(44);
+		robot.turnRight(90);
+		robot.forward(75);
 	}
 
 }
